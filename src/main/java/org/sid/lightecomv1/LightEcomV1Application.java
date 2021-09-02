aa6d9ccccc8ec6fad1f82f876d8f041b238f5954
hassen zeighoum <hassen.zeighm@gmail.com>
2021-09-02 16:21:08 +0100

    3eme commit

--- src/main/java/org/sid/lightecomv1/LightEcomV1Application.java
+++ src/main/java/org/sid/lightecomv1/LightEcomV1Application.java
@@ -1,5 +1,4 @@
 package org.sid.lightecomv1;
-
 import net.bytebuddy.utility.RandomString;
 import org.sid.lightecomv1.dao.CategoryRepository;
 import org.sid.lightecomv1.dao.ProductRepository;
@@ -9,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 
 import java.util.Random;
 
@@ -18,6 +18,8 @@ public class LightEcomV1Application implements CommandLineRunner {
     private ProductRepository productRepository;
     @Autowired
     private CategoryRepository categoryRepository;
+    @Autowired
+    private RepositoryRestConfiguration repositoryRestConfiguration;
 
 
     public static void main(String[] args) {
@@ -26,6 +28,8 @@ public class LightEcomV1Application implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        repositoryRestConfiguration.exposeIdsFor(Product.class, Category.class);
+
         categoryRepository.save(new Category(null,"Computers",null,null,null));
         categoryRepository.save(new Category(null,"Printers",null,null,null));
         categoryRepository.save(new Category(null,"Smart Phone",null,null,null));
--- src/main/java/org/sid/lightecomv1/web/CatalogueRestController.java
+++ src/main/java/org/sid/lightecomv1/web/CatalogueRestController.java
@@ -5,10 +5,12 @@ import org.sid.lightecomv1.entities.Product;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+@RestController
 public class CatalogueRestController {
     private ProductRepository productRepository;
 
@@ -19,7 +21,7 @@ public class CatalogueRestController {
     @GetMapping(path="/photoProduct/{id}", produces = MediaType.IMAGE_PNG_VALUE)
     public byte[] getPhoto(@PathVariable("id") Long id) throws Exception{
         Product p=productRepository.findById(id).get();
-        return Files.readAllBytes(Paths.get(System.getProperty("user/home")+"/ecomm/products"+p.getPhotoName()));
+        return Files.readAllBytes(Paths.get(System.getProperty("user.home")+"/ecomm/products/"+p.getPhotoName()));
     }
 
 }
